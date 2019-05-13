ae1db8ad7a90280263bf36ed112027a7f48f7c28
Peter Spiess-Knafl <dev@spiessknafl.at>
2019-05-13 13:10:18 +0200

    Moved WarehouseClient into separate class to test "codegenability"

--- examples/warehouse/main.cpp
+++ examples/warehouse/main.cpp
@@ -1,30 +1,40 @@
 #include "examples/inmemoryconnector.hpp"
 #include "warehouseapp.hpp"
 
-#include <jsonrpccxx/server.hpp>
-#include <jsonrpccxx/client.hpp>
 #include <iostream>
+#include <jsonrpccxx/client.hpp>
+#include <jsonrpccxx/server.hpp>
 
 using namespace jsonrpccxx;
 using namespace std;
+
+class WareHouseClient {
+public:
+  WareHouseClient(JsonRpcClient &client) : client(client) {}
+  bool AddProduct(const Product &p) { return client.CallMethod<bool>(1, "AddProduct", {p}); }
+  Product GetProduct(const std::string &id) { return  client.CallMethod<Product>(1, "GetProduct", {id});}
+private:
+  JsonRpcClient &client;
+};
 
 int main() {
   JsonRpc2Server rpcServer;
   InMemoryConnector connector(rpcServer);
   JsonRpcClient client(connector, version::v2);
 
-  //Bindings
+  // Bindings
+  WareHouseClient appClient(client);
   WarehouseServer app;
   rpcServer.Add("GetProduct", GetHandle(&WarehouseServer::GetProduct, app), {"id"});
   rpcServer.Add("AddProduct", GetHandle(&WarehouseServer::AddProduct, app), {"product"});
 
   Product p = {"0xff", 22.4, "Product 1", category::cash_carry};
-  cout << "Adding product: " << client.CallMethod<bool>(1, "AddProduct", {p}) << "\n";
+  cout << "Adding product: " << appClient.AddProduct(p) << "\n";
 
-  Product p2 = client.CallMethod<Product>(1, "GetProduct", {"0xff"});
+  Product p2 = appClient.GetProduct("0xff");
   cout << "Found product: " << p2.name << "\n";
   try {
-    Product p3 = client.CallMethod<Product>(1, "GetProduct", {"0xff2"});
+    Product p3 = appClient.GetProduct("0xff2");
   } catch (JsonRpcException &e) {
     cerr << "Error finding product: " << e.what() << "\n";
     return 1;
