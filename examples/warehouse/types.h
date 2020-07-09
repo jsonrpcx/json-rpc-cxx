64e69f17fab616b6ae2365fe1ad7cfbea124c28c
Richard Edgar <freesurfer.rge@gmail.com>
2020-07-09 19:41:13 -0400

    Fix types

--- examples/warehouse/types.h
+++ examples/warehouse/types.h
@@ -2,15 +2,29 @@
 #include <nlohmann/json.hpp>
 
 enum class category { order, cash_carry };
+
 struct Product {
+  Product() :
+    id(),
+    price(),
+    name(),
+    cat(order) {}
+
   std::string id;
   double price;
   std::string name;
   category cat;
 };
 
-NLOHMANN_JSON_SERIALIZE_ENUM(category, {{category::order, "order"}, {category::cash_carry, "cc"}});
-inline void to_json(nlohmann::json &j, const Product &p) { j = nlohmann::json{{"id", p.id}, {"price", p.price}, {"name", p.name}, {"category", p.cat}}; }
+NLOHMANN_JSON_SERIALIZE_ENUM(category, {{category::order, "order"}, {category::cash_carry, "cc"}})
+
+inline void to_json(nlohmann::json &j, const Product &p) {
+  j = nlohmann::json{{"id", p.id},
+		     {"price", p.price},
+		     {"name", p.name},
+		     {"category", p.cat}};
+ }
+
 inline void from_json(const nlohmann::json &j, Product &p) {
   j.at("name").get_to(p.name);
   j.at("id").get_to(p.id);
